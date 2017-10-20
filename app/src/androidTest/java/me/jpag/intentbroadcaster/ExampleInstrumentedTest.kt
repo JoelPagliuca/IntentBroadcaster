74a378703cc5d7a294d8b32448f7a4f0c8386742
Joel Pagliuca <Joel.Pagliuca@cba.com.au>
2017-10-20 12:11:55 +1100

    first commit

---
+++ app/src/androidTest/java/me/jpag/intentbroadcaster/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package me.jpag.intentbroadcaster
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("me.jpag.intentbroadcaster", appContext.packageName)
+    }
+}
