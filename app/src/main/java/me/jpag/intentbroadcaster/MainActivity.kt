0e0e5bbf8db0ccf4858ef455473efe2726098a82
Joel Pagliuca <Joel.Pagliuca@cba.com.au>
2017-10-20 15:11:59 +1100

    we are broadcasting intents

--- app/src/main/java/me/jpag/intentbroadcaster/MainActivity.kt
+++ app/src/main/java/me/jpag/intentbroadcaster/MainActivity.kt
@@ -1,12 +1,16 @@
 package me.jpag.intentbroadcaster
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.EditText
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,9 +19,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        // set the behavior for the button
+        btnRun.setOnClickListener {
+            val action : String = txtAction.text.toString()
+            Log.d(this.localClassName.toString(),"got action: "+action)
+            val intent = Intent(action)
+            sendBroadcast(intent)
         }
     }
 
@@ -31,6 +38,7 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        Log.d(this.localClassName.toString(), "onOptionsItemSelected")
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
